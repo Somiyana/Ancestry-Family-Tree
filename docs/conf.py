25c5d7e86567feb08180c59b6a288ad2b82ec676
Somiyana <somiyanamir@gmail.com>
2026-02-25 12:35:18 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,15 @@
-# -- Project information -----------------------------------------------------
 project = 'Family Tree'
-copyright = 'Family Tree'
-author = 'Your Name'
-
-# The full version, including alpha/beta/rc tags
+author = 'Family Tree'
 release = '1.0'
 
-# -- General configuration ---------------------------------------------------
-extensions = [
-    'sphinx.ext.autodoc',  # For automatic documentation generation
-    'sphinx.ext.napoleon',  # For Google-style docstrings
-]
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
 
-# The master toctree document.
-master_doc = 'index'
-
-# -- Options for HTML output -------------------------------------------------
-html_theme = 'alabaster'  # Theme to use (you can change this to 'sphinx_rtd_theme' for Read the Docs theme)
+html_theme = 'alabaster'
 html_static_path = ['_static']
 
-# -- Add custom robots.txt -----------------------------------------
-# Specify additional files to include, like robots.txt
-html_extra_path = ['robots.txt']
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '.png'
